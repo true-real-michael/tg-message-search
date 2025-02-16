fe25ca7e71a0055b11ef56fafe97373d549f2250
Mikhail Kiselyov <kiselev.0353@gmail.com>
2025-02-16 18:12:14 +0300

    load from json but an issue occurs (out of bounds)

--- src/lemmatizer.rs
+++ src/lemmatizer.rs
@@ -2,12 +2,6 @@ use flate2::read::GzDecoder;
 use std::collections::HashMap;
 use std::io::prelude::Read;
 
-macro_rules! log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
-}
-
 const DICTIONARY_RAW: &[u8] = include_bytes!("../data/lemmatization-ru.tsv.gz");
 
 #[derive(Default)]
@@ -17,7 +11,6 @@ pub struct Lemmatizer {
 
 impl Lemmatizer {
     pub fn new() -> Self {
-        log!("file length: {:?}", DICTIONARY_RAW.len());
         let mut decoder = GzDecoder::new(DICTIONARY_RAW);
         let mut data = String::new();
         decoder.read_to_string(&mut data).unwrap();
