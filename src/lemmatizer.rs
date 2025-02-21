3b0c23b172b728e4ae2d27d2f57d84e8a8cbf017
Mikhail Kiselyov <kiselev.0353@gmail.com>
2025-02-21 21:43:09 +0300

    use &\'static str in lemmatizer

--- src/lemmatizer.rs
+++ src/lemmatizer.rs
@@ -6,7 +6,7 @@ const DICTIONARY_RAW: &[u8] = include_bytes!("../data/lemmatization-ru.tsv.gz");
 
 #[derive(Default)]
 pub struct Lemmatizer {
-    dict: HashMap<String, String>,
+    dict: HashMap<&'static str, &'static str>,
 }
 
 impl Lemmatizer {
@@ -14,22 +14,23 @@ impl Lemmatizer {
         let mut decoder = GzDecoder::new(DICTIONARY_RAW);
         let mut data = String::new();
         decoder.read_to_string(&mut data).unwrap();
-        let dict: HashMap<String, String> = data
+
+        let data = Box::leak(data.into_boxed_str());
+
+        let dict: HashMap<&str, &str> = data
             .split('\n')
             .filter(|line| !line.is_empty())
             .flat_map(|line| {
                 let mut parts = line.split('\t');
-                let lemma = parts.next().unwrap().to_string();
-                parts.map(String::from).zip(std::iter::repeat(lemma))
+                let lemma = parts.next().unwrap();
+                parts.map(move |word| (word, lemma))
             })
             .collect();
+
         Self { dict }
     }
 
     pub fn lemmatize(&self, word: &str) -> String {
-        self.dict
-            .get(word)
-            .cloned()
-            .unwrap_or_else(|| word.to_string())
+        self.dict.get(word).unwrap_or(&word).to_string()
     }
 }
