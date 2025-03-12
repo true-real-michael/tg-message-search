fa6d127124b0273bcec00dbe084226c57bd8ddff
Mikhail Kiselyov <kiselev.0353@gmail.com>
2025-03-12 13:45:51 +0300

    style

--- src/components/file_input.rs
+++ src/components/file_input.rs
@@ -17,8 +17,8 @@ pub fn FileInput(set_input_data: WriteSignal<Option<String>>) -> impl IntoView {
 
     view! {
         <ol>
-            <li> Export the text data from a telegram chat according to <a href="https://telegram.org/blog/export-and-more" class="underline text-blue-600 hover:text-blue-800 visited:text-purple-700"> this page</a>.</li>
-            <li> Upload the <code>results.json</code> file from the export directory. (This app runs fully in your browser and it does not save any data.)</li>
+            <li> Export the text data from a telegram chat according to <a href="https://telegram.org/blog/export-and-more" class="underline p-1 text-blue-600 hover:text-blue-800 visited:text-purple-700"> this page</a>.</li>
+            <li> Upload the <code class="p-1">results.json</code> file from the export directory. (This app runs fully in your browser and it does not save any data.)</li>
             <li> Enter the keywords and find the relevant parts of the chat.</li>
         </ol>
         <div class="flex items-center justify-center">
@@ -29,7 +29,7 @@ pub fn FileInput(set_input_data: WriteSignal<Option<String>>) -> impl IntoView {
                         file_input.get().unwrap().click();
                     }
                 >
-                    "Upload File"
+                    "Upload results.txt"
                 </button>
                 <input
                     type="file"
